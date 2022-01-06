1ab656cc99f0dae1d273036a7ffb27e3bb651bdc
Cron3x <57831654+Cron3x@users.noreply.github.com>
2022-01-06 10:57:34 +0100

    Add files via upload

---
+++ combine-photos-to-vidio/photos.py
@@ -0,0 +1,32 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+import glob, os
+os.chdir("Photos/")
+try:
+    os.mkdir("conv/")
+except OSError as e:
+    print(e)
+
+dates_list = []
+
+for file in glob.glob("*.jpg"):
+    # path to the image or video
+    imagename = file
+
+    # read the image data using PIL
+    image = Image.open(imagename)
+
+    # extract EXIF data
+    exifdata = image.getexif()
+    mod_exif = exifdata[306].split(' ')[0].replace(':','+')
+    print(f"{mod_exif}.jpg")
+    image.save(f"conv/{mod_exif}.jpg")
+    dates_list.append(mod_exif)
+
+
+dates_list.sort()
+print(dates_list)
+
+for i in range(len(dates_list)):
+    os.rename(f"conv/{dates_list[i]}.jpg", f"conv/{i}.jpg")
